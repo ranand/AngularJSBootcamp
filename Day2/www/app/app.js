c478f66f1f4e280ca1162a6d16744fdbb26a4e30
Anand Ramakrishnan <ramak.anand@gmail.com>
2015-07-21 11:39:51 -0700

    Day2 : routeProvider, menu.html

--- Day2/www/app/app.js
+++ Day2/www/app/app.js
@@ -1,8 +1,29 @@
 // Create a module first.
-angular.module('day2', [])
+// add ngRoute dependency if using $routeProvider
+angular.module('day2', [
+  'ngRoute'
+])
   // followed by a controller.
   // inject $scope
   // don't add $scope to html file. The file should know it is coming from $scope.
   .controller('mainController', function($scope) {
     $scope.greeting = "Hello";
-  });
+  })
+  // define config phase.
+  .config(function($routeProvider){
+    // make sure to create an object inside the when call.
+    $routeProvider
+      .when('/', {
+        //template: 'This is where my menu will live.'
+        templateUrl: 'app/menu/menu.html'
+      })
+      //good habit to provide otherwise.
+      .otherwise({
+        redirectTo: '/'
+      })
+    // end $routeProvider
+    ;
+  })
+
+// leave it at the end to not forget it.
+;
